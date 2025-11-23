d001fb250b77e9ee82b0d1afa48332948f9f51e6
kalkidan Ayalew <ayalewkalkidan48@gmail.com>
2025-11-23 13:45:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <div style={{ width: "500px", margin: "0 auto", padding: "20px" }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
+
+
 function App() {
   const [count, setCount] = useState(0)
 
